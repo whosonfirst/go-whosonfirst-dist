c6a63e6d63d6bc8a89b89a593ad0fa4c52154a9c
thisisaaronland <thisisaaronland@whosonfirst.org>
2018-11-28 15:12:31 +0000

    remove old compress code

--- compress/directory.go
+++ compress/directory.go
@@ -1,14 +1,10 @@
 package compress
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/mholt/archiver"
 	_ "log"
-	"os"
-	"os/exec"
 	"path/filepath"
-	"strings"
 )
 
 // https://godoc.org/github.com/mholt/archiver#TarBz2
@@ -40,63 +36,4 @@ func CompressDirectory(source string, chroot string, opts *CompressOptions) (str
 	}
 
 	return path_dest, nil
-}
-
-func CompressDirectoryOld(source string, chroot string, opts *CompressOptions) (string, error) {
-
-	abs_source, err := filepath.Abs(source)
-
-	if err != nil {
-		return "", err
-	}
-
-	dest := fmt.Sprintf("%s.tar.bz2", abs_source)
-
-	abs_chroot, err := filepath.Abs(chroot)
-
-	if err != nil {
-		return "", err
-	}
-
-	// this bit is important - we are going to tar -C chroot
-	// so we don't want to pass an absolute path (to tar)
-
-	rel_source := strings.Replace(source, abs_chroot, "", 1)
-
-	if strings.HasPrefix(rel_source, "/") {
-		rel_source = strings.Replace(rel_source, "/", "", 1)
-	}
-
-	tar := "tar"
-
-	args := []string{
-		"-C", abs_chroot, // -C is for chroot
-		"-cjf", // -c is for create; -j is for bzip; -f if for file
-		dest,
-		rel_source,
-	}
-
-	cmd := exec.Command(tar, args...)
-
-	// to do : wire the Logger stuff in to this...
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-
-	err = cmd.Run()
-
-	if err != nil {
-		return "", err
-	}
-
-	if opts.RemoveSource {
-
-		err = os.RemoveAll(source)
-
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return dest, nil
 }
