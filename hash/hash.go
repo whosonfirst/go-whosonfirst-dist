8ab44d6ff5510a1173433cf07b0ca573285afdc1
thisisaaronland <thisisaaronland@whosonfirst.org>
2018-11-29 15:08:23 +0000

    remove old code

--- hash/hash.go
+++ hash/hash.go
@@ -4,18 +4,8 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"io"
-	"io/ioutil"
 	"os"
 )
-
-/*
-
-or this:
-
-shasum -a 256 src-whosonfirst-data-latest.db
-d515bee2e63609f9e9bf9c91dd5e7464debadd9fcd46cadf4358c7150ef85a1f  src-whosonfirst-data-latest.db
-
-*/
 
 func HashFile(path string) (string, error) {
 
@@ -37,17 +27,6 @@ func HashFile(path string) (string, error) {
 	str := hex.EncodeToString(hash[:])
 
 	return str, nil
-}
-
-func HashFileOld(path string) (string, error) {
-
-	body, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		return "", err
-	}
-
-	return HashBytes(body)
 }
 
 func HashBytes(body []byte) (string, error) {
